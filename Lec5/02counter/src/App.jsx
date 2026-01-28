423222eeaaa144c9f88ef56897ba95dfb631913a
Sukanta Mandal <mandalsukantagod@gmail.com>
2026-01-29 04:56:56 +0530

    Hooks

---
+++ Lec5/02counter/src/App.jsx
@@ -0,0 +1,41 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  let [counter,setCounter] = useState(15);
+
+  // let counter = 5;
+
+  const addValue = () =>{
+    if(counter < 20)
+    setCounter(counter+1);
+  }
+
+  const removeValue = () =>{
+    if(counter>0)
+    setCounter(counter-1);
+  }
+
+  return (
+    <>
+      <h1>Chai aur react</h1>
+      <h2>Counter Value : {counter} </h2>
+
+      <button
+      onClick={addValue}
+       >Add Value {counter} </button>
+      <br /><br />
+      <button
+      onClick={removeValue}
+      >Decrease Value {counter} </button>
+
+      <p>Footer: {counter} </p>
+     
+    </>
+  )
+}
+
+export default App
